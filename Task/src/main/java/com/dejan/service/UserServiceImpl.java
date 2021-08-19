df937a63d32eac02538bb4cd519d820bbe2066a3
Dejan Kulusic <dejan_kula@yahoo.com>
2021-08-19 16:55:17 +0200

    Update UserServiceImpl.java

--- Task/src/main/java/com/dejan/service/UserServiceImpl.java
+++ Task/src/main/java/com/dejan/service/UserServiceImpl.java
@@ -40,4 +40,324 @@ public class UserServiceImpl implements UserService{
 
 
     }
+}package com.dejan.service;
+
+import com.dejan.model.User;
+import com.dejan.repository.UserRepo;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserRepo userRepo;
+
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo =
+                userRepo;
+    }
+
+    @Override
+    public User save(User user) {
+        userRepo.save(user);
+
+        return user;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        userRepo.deleteById(id);
+    }
+
+
+    @Override
+    public List<User> findAllUser() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User findUserbyId(Long id) {
+            return userRepo.getById(id);
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+        return null;
+    }
+
+    /**
+     * Returns a {@link Page} of entities meeting the paging restriction provided in the {@code Pageable} object.
+     *
+     * @param pageable
+     * @return a page of entities
+     */
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    /**
+     * Returns the number of entities available.
+     *
+     * @return the number of entities.
+     */
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    /**
+     * Deletes the entity with the given id.
+     *
+     * @param aLong must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
+     */
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public <S extends User> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    /**
+     * Retrieves an entity by its id.
+     *
+     * @param aLong must not be {@literal null}.
+     * @return the entity with the given id or {@literal Optional#empty()} if none found.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    @Override
+    public Optional<User> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    /**
+     * Returns whether an entity with the given id exists.
+     *
+     * @param aLong must not be {@literal null}.
+     * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    /**
+     * Flushes all pending changes to the database.
+     */
+    @Override
+    public void flush() {
+
+    }
+
+    /**
+     * Saves an entity and flushes changes instantly.
+     *
+     * @param entity entity to be saved. Must not be {@literal null}.
+     * @return the saved entity
+     */
+    @Override
+    public <S extends User> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    /**
+     * Saves all entities and flushes changes instantly.
+     *
+     * @param entities entities to be deleted. Must not be {@literal null}.
+     * @return the saved entities
+     * @since 2.5
+     */
+    @Override
+    public <S extends User> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    /**
+     * Deletes the given entities in a batch which means it will create a single query. This kind of operation leaves JPAs
+     * first level cache and the database out of sync. Consider flushing the {@link EntityManager} before calling this
+     * method.
+     *
+     * @param entities entities to be deleted. Must not be {@literal null}.
+     * @since 2.5
+     */
+    @Override
+    public void deleteAllInBatch(Iterable<User> entities) {
+
+    }
+
+    /**
+     * Deletes the entities identified by the given ids using a single query. This kind of operation leaves JPAs first
+     * level cache and the database out of sync. Consider flushing the {@link EntityManager} before calling this method.
+     *
+     * @param longs the ids of the entities to be deleted. Must not be {@literal null}.
+     * @since 2.5
+     */
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    /**
+     * Deletes all entities in a batch call.
+     */
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    /**
+     * Returns a reference to the entity with the given identifier. Depending on how the JPA persistence provider is
+     * implemented this is very likely to always return an instance and throw an
+     * {@link EntityNotFoundException} on first access. Some of them will reject invalid identifiers
+     * immediately.
+     *
+     * @param aLong must not be {@literal null}.
+     * @return a reference to the entity with the given identifier.
+     * @see EntityManager#getReference(Class, Object) for details on when an exception is thrown.
+     * @deprecated use {@link JpaRepository#getById(ID)} instead.
+     */
+    @Override
+    public User getOne(Long aLong) {
+        return null;
+    }
+
+    /**
+     * Returns a reference to the entity with the given identifier. Depending on how the JPA persistence provider is
+     * implemented this is very likely to always return an instance and throw an
+     * {@link EntityNotFoundException} on first access. Some of them will reject invalid identifiers
+     * immediately.
+     *
+     * @param aLong must not be {@literal null}.
+     * @return a reference to the entity with the given identifier.
+     * @see EntityManager#getReference(Class, Object) for details on when an exception is thrown.
+     * @since 2.5
+     */
+    @Override
+    public User getById(Long aLong) {
+        return null;
+    }
+
+    /**
+     * Returns a single entity matching the given {@link Example} or {@literal null} if none was found.
+     *
+     * @param example must not be {@literal null}.
+     * @return a single entity matching the given {@link Example} or {@link Optional#empty()} if none was found.
+     * @throws IncorrectResultSizeDataAccessException if the Example yields more than one result.
+     */
+    @Override
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    /**
+     * Returns a {@link Page} of entities matching the given {@link Example}. In case no match could be found, an empty
+     * {@link Page} is returned.
+     *
+     * @param example  must not be {@literal null}.
+     * @param pageable can be {@literal null}.
+     * @return a {@link Page} of entities matching the given {@link Example}.
+     */
+    @Override
+    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    /**
+     * Returns the number of instances matching the given {@link Example}.
+     *
+     * @param example the {@link Example} to count instances for. Must not be {@literal null}.
+     * @return the number of instances matching the {@link Example}.
+     */
+    @Override
+    public <S extends User> long count(Example<S> example) {
+        return 0;
+    }
+
+    /**
+     * Checks whether the data store contains elements that match the given {@link Example}.
+     *
+     * @param example the {@link Example} to use for the existence check. Must not be {@literal null}.
+     * @return {@literal true} if the data store contains elements that match the given {@link Example}.
+     */
+    @Override
+    public <S extends User> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    /**
+     * Deletes a given entity.
+     *
+     * @param entity must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given entity is {@literal null}.
+     */
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    /**
+     * Deletes all instances of the type {@code T} with the given IDs.
+     *
+     * @param longs must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
+     * @since 2.5
+     */
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    /**
+     * Deletes the given entities.
+     *
+     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal entities} or one of its entities is {@literal null}.
+     */
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    /**
+     * Deletes all entities managed by the repository.
+     */
+    @Override
+    public void deleteAll() {
+
+    }
 }
+
